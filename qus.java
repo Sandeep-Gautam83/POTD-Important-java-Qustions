d420362f4fb745360276873697fda1c59ae6c365
Sandeep Gautam <145046164+Sandeep-Gautam83@users.noreply.github.com>
2024-06-22 14:48:19 +0530

    Add files via upload

--- qus.java
+++ qus.java
@@ -1,72 +1,14 @@
-   
- /* 
- Question 1 : Print the number of 7’s that are in the 2d array.
-   Example :
-   Input - int[][] array = { {4,7,8},{8,8,7} };
-   Output - 2
-   */
-   /* 
-   public class qus {
-        public static void main(String[] args) {
-        int[][] array = { {4, 7, 8}, {8, 8, 7} };
-        int countOf7 = 0;
-        for(int i=0; i<array.length; i++) {
-        for(int j=0; j<array[0].length; j++) {
-        if(array[i][j] == 7) {
-        countOf7++;
-        }
-        }
-        }
-        System.out.println("count of 7 is : " + countOf7);
-        }
-        }
-*/
-/* 
-Question 2 : Print out the sum of the numbers in the second row of the “nums” array.
-Example :
-Input - int[][] nums = { {1,4,9},{11,4,3},{2,2,3} };
-Output - 18
-*/
-/* 
-    public class qus {
-        public static void main(String[] args) {
-        int[][] nums = { {1,4,9},{11,4,3},{2,2,3} };
-        int sum = 0;
-        //sum of 2nd row elements
-        for(int j=0; j<nums[0].length; j++) {
-        sum += nums[1][j];
-        }
-        System.out.println("sum is : " + sum);
-        }
-        }
-        
-        */
-        
-// Question 3
-        public class qus {
-            public static void main(String[] args) {
-            int row = 2, column = 3;
-            int[][] matrix = { {2, 3, 7}, {5, 6, 7} };
-            // Display original matrix
-            printMatrix(matrix);
-            // Transpose the matrix
-            int[][] transpose = new int[column][row];
-            for(int i = 0; i<row; i++) {
-            for (int j = 0; j<column; j++) {
-            transpose[j][i] = matrix[i][j];
-            }
-            }
-            // print the transposed matrix
-            printMatrix(transpose);
-            }
-            public static void printMatrix(int[][] matrix) {
-            System.out.println("The matrix is: ");
-            for(int i=0; i<matrix.length; i++) {
-            for (int j=0; j<matrix[0].length; j++) {
-            System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-            }
-            }
-            }
-            
+public class qus {
+    public static void main(String[] args) {
+ for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+if((i+j)%2==0){
+    System.out.print(1+" ");
+}else{
+    System.out.print(0+" ");
+}
+System.out.println();
+    }
+ }
+    }
+}
